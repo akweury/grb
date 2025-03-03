388cfa5a7ab826021c98230ca95f9bb91885e229
jing <shajingyuan@me.com>
2025-03-04 00:02:40 +0100

    update Llava

--- scripts/baseline_models/llava.py
+++ scripts/baseline_models/llava.py
@@ -54,6 +54,7 @@ def infer_logic_rules(model, processor, train_positive, train_negative, device, 
     print(f"train_positive: {train_positive}, train_negative: {train_negative}")
     inputs = processor(text=prompt, images=train_positive + train_negative, return_tensors="pt").to(device)
     print(f"logic input:{inputs}")
+    print(inputs["pixel_values"].shape)  # Should be (batch_size, 3, H, W)
     outputs = model.generate(**inputs, max_new_tokens=1000)
     logic_rules = processor.tokenizer.decode(outputs[0], skip_special_tokens=True)
     print(f"Inferred rules for {principle}: {logic_rules}")
