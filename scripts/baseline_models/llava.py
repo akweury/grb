0df572e687d7cb467e548a0c211591fe3989f529
jing <shajingyuan@me.com>
2025-03-04 11:28:14 +0100

    update Llava

--- scripts/baseline_models/llava.py
+++ scripts/baseline_models/llava.py
@@ -130,7 +130,8 @@ def infer_logic_rules(model, processor, train_positive, train_negative, device, 
     # print(inputs)
     generate_ids = model.generate(**inputs, max_new_tokens=1024)
     answer = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
-    print(f"Train Logic Rules: {answer}\n\n")
+    logic_rules = answer.split('assistant\n')[-1]
+    print(f"Train Logic Rules: {logic_rules}\n\n")
     return answer
 
 
@@ -163,8 +164,8 @@ def evaluate_llm(model, processor, test_images, logic_rules, device, principle):
 
         generate_ids = model.generate(**inputs, max_new_tokens=1024)
         prediction = processor.decode(generate_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
-
-        print(f"Prediction: {prediction}\n\n"
+        prediction_result = prediction.split('assistant')[-1]
+        print(f"Prediction: {prediction_result}\n\n"
               f"Logic Rules: {logic_rules}\n\n")
         predicted_label = 1 if "positive" in prediction else 0
         all_labels.append(label)
