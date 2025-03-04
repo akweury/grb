4f8f863241292557380610b279507fbf11e1750c
jing <shajingyuan@me.com>
2025-03-04 11:25:04 +0100

    update Llava

--- scripts/baseline_models/llava.py
+++ scripts/baseline_models/llava.py
@@ -128,7 +128,7 @@ def infer_logic_rules(model, processor, train_positive, train_negative, device, 
 
     # Generate
     # print(inputs)
-    generate_ids = model.generate(**inputs, max_new_tokens=20)
+    generate_ids = model.generate(**inputs, max_new_tokens=1024)
     answer = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
     print(f"Train Logic Rules: {answer}\n\n")
     return answer
@@ -161,7 +161,7 @@ def evaluate_llm(model, processor, test_images, logic_rules, device, principle):
             return_tensors="pt"
         ).to(model.device, torch.float16)
 
-        generate_ids = model.generate(**inputs, max_new_tokens=10)
+        generate_ids = model.generate(**inputs, max_new_tokens=1024)
         prediction = processor.decode(generate_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
 
         print(f"Prediction: {prediction}\n\n"
