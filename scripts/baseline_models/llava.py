ed771873fa6488ce5118ba155fadfe1fe4ea8801
jing <shajingyuan@me.com>
2025-03-04 11:36:02 +0100

    update Llava

--- scripts/baseline_models/llava.py
+++ scripts/baseline_models/llava.py
@@ -130,8 +130,8 @@ def infer_logic_rules(model, processor, train_positive, train_negative, device, 
     # print(inputs)
     generate_ids = model.generate(**inputs, max_new_tokens=1024)
     answer = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
-    logic_rules = answer.split('assistant\n')[-1]
-    print(f"Train Logic Rules: {logic_rules}\n\n")
+    for i in range(len(answer)):
+        print(f"Train Logic Rules {i}: {answer[i]}\n\n")
     return answer
 
 
@@ -164,9 +164,10 @@ def evaluate_llm(model, processor, test_images, logic_rules, device, principle):
 
         generate_ids = model.generate(**inputs, max_new_tokens=1024)
         prediction = processor.decode(generate_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
-        prediction_result = prediction.split('assistant')[-1]
-        print(f"Prediction: {prediction_result}\n\n"
-              f"Logic Rules: {logic_rules}\n\n")
+        for i in range(len(prediction)):
+
+            print(f"Prediction {i}: {prediction[i]}\n\n"
+                  f"Logic Rules {i}: {logic_rules}\n\n")
         predicted_label = 1 if "positive" in prediction else 0
         all_labels.append(label)
         all_predictions.append(predicted_label)
